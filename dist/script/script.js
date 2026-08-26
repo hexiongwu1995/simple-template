22285430140103c2941edc9f9de5e5205b11e346
hexiongwu1995_local <hexiongwu1995@outlook.com>
2026-08-27 04:49:04 +0800

    feature分支：修复了mathkax的块级公式超出显示区域的问题

--- dist/script/script.js
+++ dist/script/script.js
@@ -7,9 +7,16 @@ const getLevel = (el) => parseInt(el.tagName[1], 10);
 
 // 给每个 heading 生成id、添加numbering和data-original-text属性
 function initHeadings() {
-  headings = Array.from(article.querySelectorAll("h1,h2, h3, h4"));
+  headings = Array.from(article.querySelectorAll("h2, h3, h4"));
+
   const counters = [];
+
   if (headings.length > 0) {
+
+    // if (getLevel(headings[0]) > getLevel(headings[1])) {
+    //   alert("初始标题的层级必须小于等于后续标题的层级");
+    // }
+
     headings.forEach((heading) => {
       const level = getLevel(heading);
 
